14e41ff4ea06de12d40eae3d1b4c8f2b62f830a8
npsaunders <neilpsaunders@gmail.com>
2022-09-19 22:34:02 -0400

    update component styles

--- src/containers/Sentiment.js
+++ src/containers/Sentiment.js
@@ -1,9 +1,11 @@
 function Sentiment(props) {
   return (
-    <div id="sentiment">Sentiment
-      <div id="num1">{props.num1}</div>
-      <div id="num2">{props.num2}</div>
-      <div id="num3">{props.num3}</div>
+    <div id="sentiment">Sentiment Analysis
+      <div id="sent">
+        <div id="sent_num1">{props.num1}</div>
+        <div id="sent_num2">{props.num2}</div>
+        <div id="sent_num3">{props.num3}</div>
+      </div>
     </div>
   );
 }
